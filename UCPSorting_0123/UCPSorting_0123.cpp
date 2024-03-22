419b2aa4ae99ae263ec4ca3c228407c543a42a3e
CaptainAAI <anandaagungismail@gmail.com>
2024-03-22 10:40:33 +0700

    Library default and declaration

--- UCPSorting_0123/UCPSorting_0123.cpp
+++ UCPSorting_0123/UCPSorting_0123.cpp
@@ -1,20 +1,71 @@
-// UCPSorting_0123.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// 1) Insertion sort 
+	// to sort this list by using the insertion sort algorithm, we need to use temp/reserve to store the index value 
+	// that is intended to compare other index value number, and in order to move the bigger number we need to use temp in order to 
+	// make the index empty, and the other value can move to the other index
+	// 
+	// example, we have 5 index here
+	// 
+	//  0  1  2  3  4
+	// |70|80|30|10|20|
+	// we must always to start the comparison from index 1 (that has value of 80) 
+	// |70|80|30|10|20|
+	// |70|  |30|10|20|   -> store the 80 to temp.
+	// and then compare to the left index value
+	//			if the left index value is smaller or equal it doesn't need to move to right index
+	//			
+	//			if the left index value is bigger then it will need to move the value from left index to right index
+	// now lets jump to comparison for index 2
+	// |70|80|  |10|20|  -> store the 30 to temp
+	// |70|  |80|10|20|
+	// |  |70|80|10|20|
+	// |30|70|80|10|20|  -> place the 30 to index 0
+	// same thing is also applied for index number 3 and 4
 
-#include <iostream>
+	// here's the algorithm for insertion sort : 
 
-int main()
-{
-    std::cout << "Hello World!\n";
-}
+// 1. Repeat steps 2, 3, 4, and 5 varying i from 1 to n – 1
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+// 2. Set temp = arr[i]
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+// 3. Set j = i – 1
+
+// 4. repeat until j becomes less than 0 or arr[j] becomes less than or equal to temp :
+// 4a. Shift the value at index j to index j + 1
+// 4b. Decrement j by 1
+
+// 5. Store temp at index j + 1
+//===================================================================================================================================
+//	2) Selection Sort
+
+	//suitable for sorting small lists only
+	//selects the smallest one in each scan and moves the value to its correct position 
+	// 
+	// |105|120|10|200|20| here we have 5 index
+	// 
+	// it will look for the smallest one that is 10 in index no 2 and then it will swap with the first index ( index no 0 )
+	// 
+	//  
+	//  |105|120|10|200|20| -> |10|120|105|200|20|
+	// it will continue until the last pass 
+	// last pass |10|20|105|120|200|
+
+	// here's the algorithm for selection sort
+
+// 1. Repeat steps 2 and 3 varying j from 0 to n – 2
+// 2. Find the minimum value in arr[j] to arr[n – 1]:
+//   a. Set min_index = j
+//   b.  Repeat step c varying i from j + 1 to n – 1
+//   c. If arr[i] < arr[min_index] :
+//        i.min_index = i
+// 3. Swap arr[j] with arr[min_index]
+//=========================================================================================================================
+// 3) to find how many steps that is required in sort algorithm, we need this formula
+// Total number of comparisons = (n – 1) + (n – 2) + (n – 3) + … + 3 + 2 + 1 = n(n – 1) / 2.
+// number of pass is always n-1 
+//============================================================================================================================================
+//4. 
+#include<iostream>
+using namespace std;
+
+int n;
+int Agung[23];
